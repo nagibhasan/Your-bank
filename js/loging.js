27a8fae8875ac44de7e5e9350bf842b0bbd80600
nagibhasan <nagibhasan132@gmail.com>
2023-02-13 01:46:05 +0600

    forth commit

--- js/loging.js
+++ js/loging.js
@@ -4,5 +4,19 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     // alway remember to use .value to get text from an input field
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
-    console.log(email);
+    // step 3: get password
+    //a: set id an the html element
+    //b: get the element
+    //c: get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    
+    //DANGER: DO NOT VERIFY email password on the client side
+    // step-4: verify email and password and check whether valid user or not
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        console.log('valid user')
+    }
+    else{
+        console.log('invalid user')
+    }
 })
